bb65b88c5bd72df484746e0f22b12c82338e9c4d
Suzy Kenne <suzyndatewo@gmail.com>
2025-01-21 16:06:35 +0100

    updated pages layouts

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,18 @@
-<h1>The List of Tasks</h1>
+@extends('layouts.app')
 
-<div>
+@section('title', 'The list of tasks')
+
+@section('content')
+    @forelse ($tasks as $task)
+            <div>
+                <a href="{{ route('task.show', ['id' => $task->id])}}">{{ $task->title}}</a>
+            </div>
+            @empty
+            <div>There are no tasks!</div>
+            @endforelse
+@endsection
+
+
     
-        @forelse ($tasks as $task)
-        <div>
-            <a href="{{ route('task.show', ['id' => $task->id])}}">{{ $task->title}}</a>
-        </div>
-        @empty
-        <div>There are no tasks!</div>
-        @endforelse
+        
    
-</div>
