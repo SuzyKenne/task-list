45bf4099ec6c65d5174977d35fd63131574c454a
Suzy Kenne <suzyndatewo@gmail.com>
2025-01-22 17:08:46 +0100

    implementated CRUD endpoint for task

---
+++ resources/views/create.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('title', 'Add Task')
+
+@section('styles')
+<style>
+    .error-message {
+        color: red;
+        font-size: 0.8rem;
+    }
+    .success-message {
+        color: green;
+        font-size: 0.8rem;
+    }
+</style>
+
+@endsection
+
+@section('content')
+   
+    <form method="POST" action="{{ route('tasks.store') }}">
+        @csrf
+        <div>
+            <label for="title">Title</label>
+            <input text="text" name="title" id="title"/>
+            @error('title')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div>
+            <label for="description">Description</label>
+            <textarea name="description" id="description" rows="5"></textarea>
+            @error('description')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div>
+            <label for="long_description">Description</label>
+            <textarea name="long_description" id="long_description" rows="10"></textarea>
+            @error('long_description')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+        
+        <div>
+            <button>Add task</button>
+        </div>
+    </form>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Http\Response;
+use App\Models\Task;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Requests\TaskRequest;
 
 
 
@@ -13,14 +14,52 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/tasks', function ()  {
     return View('index', [
-        'tasks' => \App\Models\Task::latest()->get(),
+        'tasks' => Task::latest()->where('completed', true)->get(),
     ]);
 })->name('task.index');
 
+Route::view('/tasks/create', 'create' )->name('tasks.create');
 
-Route::get('/tasks/{id}', function($id)  {
+Route::get('/tasks/{task}/edit', function(Task $task)  {
  
+   return View('edit', [ 'task' => $task]);
+  })->name('task.edit');
+
+Route::get('/tasks/{task}', function(Task $task)  {
+ 
+  return View('show', [ 'task' => $task]);
+})->name('tasks.show');
+
+
+
+
+Route::post('/tasks', function (TaskRequest $request) {
   
 
-  return View('show', [ 'task' => \App\Models\Task::findOrFail($id)]);
-})->name('task.show');
+  $task = Task::create($request->validated());
+
+
+  return redirect()->route('tasks.show', ['task' => $task->id])->with('success', 'Task created successfully!');
+})->name('tasks.store');
+
+
+
+
+Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
+ 
+  // $data = ;
+  // $task = Task::findOrFail($id);
+  // $task->title = $data['title'];
+  // $task->description = $data['description'];
+  // $task->long_description = $data['long_description'];
+
+  $task->update($request->validated());
+
+  return redirect()->route('tasks.show', ['task' => $task->id])->with('success', 'Task updated successfully!');
+})->name('tasks.update');
+
+Route::delete('/tasks/{task}', function(Task $task){
+  $task->delete();
+
+  return redirect()->route('task.index')->with('success', 'Task deleted successfully!');
+})->name('tasks.destroy');
