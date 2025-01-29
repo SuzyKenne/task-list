e08ccec441371d3c60c510f33d6a32143105e815
Suzy Kenne <suzyndatewo@gmail.com>
2025-01-29 20:35:25 +0100

    added task-toggle route

--- routes/web.php
+++ routes/web.php
@@ -63,3 +63,9 @@ Route::delete('/tasks/{task}', function (Task $task) {
 
   return redirect()->route('task.index')->with('success', 'Task deleted successfully!');
 })->name('tasks.destroy');
+
+Route::put('tasks/{task}/toggle-complete', function (Task $task) {
+  $task->toggleComplete();
+
+  return redirect()->back()->with('success', 'Task updated successfully!');
+})->name('tasks.toggle-complete');
