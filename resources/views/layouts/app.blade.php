399b60381733e462831649ecf1d17624b0354d16
Suzy Kenne <suzyndatewo@gmail.com>
2025-02-10 21:04:44 +0100

    defined style classes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+
+    {{-- blade-formatter-disable --}}
+
+    <style type="text/tailwindcss">
+    .btn{
+        @apply rounded-md px-2 py-1 text-center font-medium shadow-sm ring-1 text-slate-700  ring-slate-700/10 hover:bg-slate-50;
+    }
+    .link{
+        @apply font-medium text-gray-700 underline decoration-pink-500;
+    }
+
+    label{
+        @apply block uppercase text-slate-700 mb-2
+    }
+    input, textarea {
+        @apply shadow-sm appearance-none border w-full py-2 px-3 text-slate-700 leading-tight focus:outline-none
+    }
+
+    .error{
+        @apply text-red-500 text-sm;
+    }
+    </style>
+    {{-- blade-formatter-enable --}}
+
+
     <title class="">Laravel 10 Task List App</title>
     @yield('styles')
 </head>
